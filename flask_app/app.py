afd1dc43dbad84b2e4c30fd94d1ba6064524b980
weeiner <71634275+weeiner@users.noreply.github.com>
2025-09-06 10:41:07 +0800

    fix: fixed and setup trivia.py

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,20 +2,23 @@ import logging
 import socket
 from routes import app
 from routes.ticketing_agent import ticketing_bp
+from routes.trivia import trivia
 
 
 logger = logging.getLogger(__name__)
 
-@app.route('/', methods=['GET'])
+
+@app.route("/", methods=["GET"])
 def default_route():
-    return 'Python Template'
+    return "Python Template"
+
+
 app.register_blueprint(ticketing_bp)
 
 
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
@@ -23,7 +26,7 @@ logger.setLevel(logging.INFO)
 if __name__ == "__main__":
     logging.info("Starting application ...")
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(('localhost', 8080))
+    sock.bind(("localhost", 8080))
     port = sock.getsockname()[1]
     sock.close()
     app.run(port=port)
